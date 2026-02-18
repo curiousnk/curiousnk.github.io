ffa1f30b245eb6348f441bc9efbf0eac5e7c57f7
Nitesh Kumar <niteshk@Niteshs-MacBook-Pro.local>
2026-02-18 07:37:05 +0100

    change weather

--- zapzap/data.js
+++ zapzap/data.js
@@ -3,5 +3,5 @@ const ZAPZAP_USERS = [
   { id: "10001", name: "Alex Morgan", temperature: 32, planType: "individual", weatherConditions: "Winter", cityName: "Cold City" },
   { id: "10002", name: "Jordan Lee", temperature: 70, planType: "family", weatherConditions: "Spring", cityName: "Springfield" },
   { id: "10003", name: "Sam Chen", temperature: 85, planType: "internetbundle", weatherConditions: "Summer", cityName: "Summerville" },
-  { id: "10004", name: "Riley Davis", temperature: 70, planType: "tvinternetbundle", weatherConditions: "Fall", cityName: "Autumn Hills" }
+  { id: "10004", name: "Riley Davis", temperature: 65, planType: "tvinternetbundle", weatherConditions: "Fall", cityName: "Autumn Hills" }
 ];
